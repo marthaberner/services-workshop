49f96734a75969d13841b96176f7bbd2abe0b180
Adriana Galvez <vntgh@yahoo.com>
2016-08-17 10:39:43 -0600

    messages are displayed and user can add new message

--- controllers/post_message_controller.js
+++ controllers/post_message_controller.js
@@ -1,4 +1,17 @@
-app.controller('PostMessageController', function ($scope) {
+app.controller('PostMessageController', function ($scope, MessagesService, $location) {
   $scope.vw = {};
   $scope.vw.success = "Success!!"
+  $scope.addNew = function(message){
+    var newid = MessagesService.all.length+1
+    var send = {
+      id: newid,
+      text: message
+    }
+    MessagesService.add(send)
+  }
+  $scope.form = {}
+  $scope.form.message = ''
+  $scope.home = function(){
+    $location.path('/')
+  }
 })
